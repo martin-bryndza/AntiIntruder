f36a6983be3c253a251add0d683497afcdbfdffa
bryndza <martin.bryndza@ysoft.com>
2015-04-12 19:27:44 +0200

    Removed mapping for /**

--- AntiIntruder-Frontend/src/main/java/eu/bato/anyoffice/frontend/web/controllers/MainController.java
+++ AntiIntruder-Frontend/src/main/java/eu/bato/anyoffice/frontend/web/controllers/MainController.java
@@ -18,9 +18,9 @@ public class MainController {
         return "redirect:/";
     }
 
-    @RequestMapping("/**")
-    public String notFound() {
-        return "errors/404";
-    }
+//    @RequestMapping("/**")
+//    public String notFound() {
+//        return "errors/404";
+//    }
 
 }
