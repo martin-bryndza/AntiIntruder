cc6c544b79305897408b63bfc1213322e2d6a123
martin-bryndza <bryndza.martin@gmail.com>
2015-04-13 01:13:23 +0200

    Added link to download client

--- AntiIntruder-Frontend/src/main/java/eu/bato/anyoffice/frontend/web/controllers/MainController.java
+++ AntiIntruder-Frontend/src/main/java/eu/bato/anyoffice/frontend/web/controllers/MainController.java
@@ -1,5 +1,12 @@
 package eu.bato.anyoffice.frontend.web.controllers;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,10 +19,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class MainController {
+    
+    @Autowired
+    Environment env;
 
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
     public String logout(@ModelAttribute CsrfToken csrf) {
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/downloadClient", method = RequestMethod.GET)
+    public void downloadClient(HttpServletResponse response) throws FileNotFoundException, IOException {
+            try (InputStream is = new FileInputStream(env.getProperty("client.path"))) {
+                response.setContentType("application/octet-stream");
+                response.setHeader("Content-Disposition", "attachment; filename=AnyOffice_client.zip");
+                org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+                response.flushBuffer();
+                is.close();
+            }
     }
 //
 //    @RequestMapping("/error")
